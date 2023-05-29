72a9da0f3f2b420274cb1fd707a9563500515101
mohammad <31886038+mohammadsedghi@users.noreply.github.com>
2023-05-29 14:14:17 +0330

    hibernate util class is written

--- src/main/java/base/repository/util/HibernateUtil.java
+++ src/main/java/base/repository/util/HibernateUtil.java
@@ -1,4 +1,17 @@
 package base.repository.util;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 public class HibernateUtil {
+    private static final StandardServiceRegistry ssr=new StandardServiceRegistryBuilder()
+            .configure("hibernate.cfg.xml").build();
+    private static final Metadata metadata=new MetadataSources(ssr).getMetadataBuilder().build();
+    private static final SessionFactory sessionFactory= metadata.getSessionFactoryBuilder().build();
+    public static SessionFactory getSessionFactory(){
+        return sessionFactory;
+    }
 }
