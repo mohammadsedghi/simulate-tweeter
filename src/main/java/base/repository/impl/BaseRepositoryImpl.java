04375c002b39c32843affd5663515453bad2f0d8
mohammad <31886038+mohammadsedghi@users.noreply.github.com>
2023-05-29 14:24:07 +0330

    BaseRepositoryImpl class is written

--- src/main/java/base/repository/impl/BaseRepositoryImpl.java
+++ src/main/java/base/repository/impl/BaseRepositoryImpl.java
@@ -2,29 +2,43 @@ package base.repository.impl;
 
 import base.entity.BaseEntity;
 import base.repository.BaseRepository;
+import base.repository.util.HibernateUtil;
+import org.hibernate.Session;
 
 import java.io.Serializable;
 import java.util.List;
 
-public class BaseRepositoryImpl <E extends BaseEntity<ID>,ID extends Serializable>
+public abstract class BaseRepositoryImpl <E extends BaseEntity<ID>,ID extends Serializable>
         implements BaseRepository<E,ID> {
+    private Session session= HibernateUtil.getSessionFactory().openSession();
     @Override
     public E save(E entity) {
-        return null;
+        session.getTransaction().begin();
+        session.persist(entity);
+        session.getTransaction().commit();
+        return entity;
     }
 
     @Override
     public E update(E entity) {
-        return null;
+        session.getTransaction().begin();
+        session.merge(entity);
+        session.getTransaction().commit();
+        return entity;
     }
 
     @Override
     public void remove(E entity) {
+        session.getTransaction().begin();
+        session.remove(entity);
+        session.getTransaction().commit();
 
     }
 
     @Override
     public List<E> load() {
-        return null;
+
+       return session.createQuery("from "+getEntityClass().getSimpleName(),getEntityClass()).getResultList();
     }
+    public abstract Class<E> getEntityClass();
 }
