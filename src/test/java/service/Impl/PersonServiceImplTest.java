e2da1053131516a5e861e3dbf901dec429173520
mohammad <31886038+mohammadsedghi@users.noreply.github.com>
2023-06-07 22:57:26 +0330

    test class for PersonServiceImplTest is created

---
+++ src/test/java/service/Impl/PersonServiceImplTest.java
@@ -0,0 +1,17 @@
+package service.Impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void signUp() {
+    }
+}
